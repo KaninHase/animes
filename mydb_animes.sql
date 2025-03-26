135db29b635b297ce4ef28356043ba911bf71d6d
KaninRaben <97570748+KaninHase@users.noreply.github.com>
2025-03-25 20:42:40 -0600

    Add files via upload

---
+++ mydb_animes.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: mydb
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `animes`
+--
+
+DROP TABLE IF EXISTS `animes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animes` (
+  `AnimeID` int DEFAULT NULL,
+  `AnimeName` varchar(255) DEFAULT NULL,
+  `Genre` varchar(255) DEFAULT NULL,
+  `ReleaseDate` date DEFAULT NULL,
+  `seasons` varchar(10) DEFAULT NULL,
+  `Chapter` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `animes`
+--
+
+LOCK TABLES `animes` WRITE;
+/*!40000 ALTER TABLE `animes` DISABLE KEYS */;
+INSERT INTO `animes` VALUES (1,'Desu Noto','Police/Mistery','2006-10-04','2','38'),(2,'Tokyo Revengers','Shounen/Romance','2021-04-01','4','+200'),(3,'Kimetsu no Yaiba','History/Shounen','2019-04-04','5','+200'),(4,'Shingeki no Kyojin','Powers/History','2013-04-07','4','139'),(5,'Tokyo Ghoul','Shounen/Mistery','2006-10-04','3','325'),(6,'Psycho Pass','Police/Mistery','2006-10-04','3','38');
+/*!40000 ALTER TABLE `animes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-25 20:41:19
